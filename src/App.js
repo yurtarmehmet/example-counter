7acf38a09e9e3521c42c5fa343966b70ffcb96f4
Mehmet Yurtar <mehmetyurtar@gmail.com>
2020-04-18 14:56:06 +0300

    counte example finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    degistir = (operation) => {
+        this.setState({
+            count: operation === "azalt" ? this.state.count - 1 : this.state.count +1
+        })
+    }
+
+    render(){
+        const {count} = this.state;
+        return <div>
+            <h1>{count}</h1>
+            <button onClick={() => {this.degistir("azalt")}}>
+                -
+            </button>
+            <button onClick={() => {this.degistir("artir")}}>
+                +
+            </button>
+        </div>
+    }
 }
 
 export default App;
